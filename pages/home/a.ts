2ed4fb64e5caab946c9942d7ccb60845f9a4f145
zhangjing <v_zhangjing15@baidu.com>
2019-08-30 11:40:45 +0800

    add template

---
+++ pages/home/a.ts
@@ -0,0 +1,9 @@
+interface Mytype {
+    aaa: number;
+}
+
+function bb(aa: string) {
+    console.log(22);
+}
+
+let a = 99;
---
+++ pages/home/home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export default class extends React.Component {
+    public render() {
+
+        return <div>
+
+            This is Home page! 9900
+
+
+        </div>;
+
+    }
+
+}
